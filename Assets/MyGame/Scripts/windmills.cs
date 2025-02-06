5a4e1c06d3417bd49a4eba3be9d905769a4ef1ba
mdinchtl <medine.dinc07@htl-salzburg.ac.at>
2025-02-06 09:28:05 +0100

    created buttons without function

---
+++ Assets/MyGame/Scripts/windmills.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class windmills : MonoBehaviour
+{
+    public GameObject windmills[] windmill; 
+    public button Button;
+
+
+
+}
